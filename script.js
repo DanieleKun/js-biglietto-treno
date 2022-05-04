5077e2aab1e449d5a952cf74f8b1631b64718b8e
Daniele <pucciodaniele95@gmail.com>
2022-05-04 16:39:58 +0200

    finish program

--- script.js
+++ script.js
@@ -4,8 +4,10 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 
-// Prezzo biglietto standard
-const ticketPrice = 0.21;
+// Prezzo biglietti
+const priceKm = 0.21;
+const priceUnder = 0.21 * 80 / 100;
+const priceOld = 0.21 * 60 / 100;
 
 // Chiedere km da percorrere
 const kmToGo = parseInt(prompt('Quanti km devi percorrere?'))
@@ -14,6 +16,23 @@ console.log(kmToGo);
 // Chiedere età
 const age = parseInt(prompt('Quanti anni hai?'))
 console.log(age);
-// Somma prezzo bilgietto e km
+
+// Somma prezzo bilgietto, km e età
+let ticketPrice;
+
+if (age < 18) {
+    ticketPrice = kmToGo * priceUnder;
+    // ticketPrice = ticketPrice.toFixed(2);
+    console.log(ticketPrice);
+} else if (age >= 65) {
+    ticketPrice = kmToGo * priceOld
+    // ticketPrice = ticketPrice.toFixed(2);
+    console.log(ticketPrice);
+} else {
+    ticketPrice = kmToGo * priceKm;
+    console.log(ticketPrice);
+}
+ticketPrice = ticketPrice.toFixed(2);
 
 // Output prezzo finale
+document.getElementById('ticketprice').innerHTML = 'Il costo del biglietto è di €' + ticketPrice
